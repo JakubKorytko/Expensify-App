48f2c5dea554e8d5054958160cf9bd1dc459c25a
Scott Deeter <scott@expensify.com>
2026-02-10 14:46:57 -0800

    Remove Parser since it's unused

--- src/libs/ModifiedExpenseMessage.ts
+++ src/libs/ModifiedExpenseMessage.ts
@@ -14,7 +14,6 @@ import {getEnvironmentURL} from './Environment/Environment';
 // eslint-disable-next-line @typescript-eslint/no-deprecated
 import {formatList, translateLocal} from './Localize';
 import Log from './Log';
-import Parser from './Parser';
 import {getPersonalDetailByEmail} from './PersonalDetailsUtils';
 import {getCleanedTagName, getPolicy, getSortedTagKeys, isPolicyAdmin} from './PolicyUtils';
 import {getOriginalMessage, isModifiedExpenseAction} from './ReportActionsUtils';
