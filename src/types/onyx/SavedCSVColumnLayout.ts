75754cdcb45c8deeb8bf863c8a60bfa99f8b59ee
alberto <alberto@expensify.com>
2026-02-11 11:08:05 +0100

    missing comments

--- src/types/onyx/SavedCSVColumnLayout.ts
+++ src/types/onyx/SavedCSVColumnLayout.ts
@@ -1,20 +1,32 @@
 /** Column mapping indexes for a CSV import (values are column index as numbers, or false if not mapped) */
 type ColumnMappingIndexes = {
+    /** Date column index or false if not mapped */
     date?: number | boolean;
+    /** Merchant column index or false if not mapped */
     merchant?: number | boolean;
+    /** Amount column index or false if not mapped */
     amount?: number | boolean;
+    /** Category column index or false if not mapped */
     category?: number | boolean;
+    /** Ignore column index or false if not mapped */
     ignore?: number | boolean;
+    /** Type (debit/credit) column index or false if not mapped */
     type?: number | boolean;
 };
 
 /** Column mapping names for a CSV import (values are header names, or false if not mapped) */
 type ColumnMappingNames = {
+    /** Date column header name or false if not mapped */
     date?: string | boolean;
+    /** Merchant column header name or false if not mapped */
     merchant?: string | boolean;
+    /** Amount column header name or false if not mapped */
     amount?: string | boolean;
+    /** Category column header name or false if not mapped */
     category?: string | boolean;
+    /** Ignore column header name or false if not mapped */
     ignore?: string | boolean;
+    /** Type (debit/credit) column header name or false if not mapped */
     type?: string | boolean;
 };
 
